3c9b8b442bebfab80eedcd438da7ca097946c3c1
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-03-01 20:34:38 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 
 app.use(express.json());
 
-app.get('/', (req, res)) {
-  res.send('Hello world')
-}
+app.get('/', (req, res) => {
+  res.send('Hello world');
+})
 
 const port = process.env.PORT || '5000';
 app.listen(port, () => console.log('Server running on port ${port}'));
