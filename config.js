2f482bdd25b1c5f266e5e1e2aeb23900710ebf7f
weiiyii <cwy.sophiechen@knights.ucf.edu>
2021-03-04 22:05:23 -0500

    mongodb connected

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 module.exports = {
   // need mongodb connection string
-  MOGODB: "",
+  MONGODB:
+    "mongodb+srv://sophie:qawsed123@cluster0.e8eca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
   // some other password...
 };
--- server.js
+++ server.js
@@ -1,17 +1,28 @@
 const express = require("express");
-const bodyParser = require("body-parser");
 const cors = require("cors");
-const app = express();
 const mongoose = require("mongoose");
-const uri = process.env.MONGODB_URI;
+const MongoClient = require("mongodb").MongoClient;
 
+// relative imports
+// db uri, mongodb connection string
 const { MONGODB } = require("./config.js");
+// const uri = process.env.MONGODB_URI;
 
+const app = express();
+const port = process.env.PORT || "5000";
+
+app.use(cors());
+// body parser included in express
 app.use(express.json());
+
+mongoose.connect(MONGODB, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.once("open", () => {
+  console.log("MongoDB Connected");
+});
 
 app.get("/", (req, res) => {
   res.send("Hello world");
 });
 
-const port = process.env.PORT || "5000";
-app.listen(port, () => console.log("Server running on port ${port}"));
+app.listen(port, () => console.log(`Server running on port ${port}`));
