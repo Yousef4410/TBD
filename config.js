3b4252983ea4ecbafc5cba64ab9ec4776381f16a
weiiyii <cwy.sophiechen@knights.ucf.edu>
2021-03-04 22:36:36 -0500

    mongodb connected: connection string needed

--- config.js
+++ config.js
@@ -1,6 +1,5 @@
 module.exports = {
   // need mongodb connection string
-  MONGODB:
-    "mongodb+srv://sophie:qawsed123@cluster0.e8eca.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  MONGODB: "",
   // some other password...
 };
