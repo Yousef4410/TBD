94deb0a66d3f40e10e110433dfff35de0ebe2b2b
TheRider98 <61335426+TheRider98@users.noreply.github.com>
2021-04-02 21:34:43 -0400

    Update posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Post = require('../models/Post');
-const validatePostInput = require("../util/validators");
+const {validatePostInput} = require("../util/post_valid");
 
 // get all posts
  router.get('/get', async (req,res) => {
