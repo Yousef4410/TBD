f14e1ff26d66279bb59545a8f1904272de7261b9
weiiyii <cwy.sophiechen@knights.ucf.edu>
2021-04-11 20:57:57 -0400

    trying to fix build path

--- server.js
+++ server.js
@@ -14,13 +14,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // for production
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("frontend/build"));
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("frontend/build"));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
-}
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+//   });
+// }
 
 app.use(cors());
 // body parser included in express
