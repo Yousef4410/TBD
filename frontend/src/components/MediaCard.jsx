31efe7b8e9292d7e112d70f1f738d412ef2c796e
Yousef Edwards <yousefedwards@gmail.com>
2021-04-17 05:09:15 -0400

    added styling to marketplace

--- frontend/src/components/MediaCard.jsx
+++ frontend/src/components/MediaCard.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import Card from '@material-ui/core/Card'
+import CardActionArea from '@material-ui/core/CardActionArea'
+import CardActions from '@material-ui/core/CardActions'
+import CardContent from '@material-ui/core/CardContent'
+import CardMedia from '@material-ui/core/CardMedia'
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
 import { TestData } from '../TestData'
-import { Grid } from '@material-ui/core';
+import { Grid } from '@material-ui/core'
+import me from '../images/me3.jpg'
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1,
-    padding: theme.spacing(2),
+    padding: theme.spacing(10),
+    marginTop: "5em",
     minWidth: "200px",
   },
   media: {
-    height: 140,
+    height: "200px",
   },
+  btn: {
+    color: "#2C98F0"
+  },
+  card: {
+    backgroundColor: "rgba(86,246,228,.10)"
+  }
 }))
 
 
@@ -34,28 +42,30 @@ export default function MediCaCard() {
         container spacing={2}>
           {TestData.map((data, key) => {
             return (
-              <Grid item xs={12} sm={6} md={4} key={key}>
-                <Card variant="outlined">
+              <Grid item xs={12} sm={6} md={3} key={key}>
+                <Card variant="outlined" className={classes.card}>
                   <CardActionArea>
                     <CardMedia
                       className={classes.media}
-                      image="/static/images/cards/contemplative-reptile.jpg"
+                      image={me}
                     />
-                    <CardContent>
+                      <CardContent>
                       <Typography gutterBottom variant="h5">
                         {data.title}
                       </Typography>
                       <Typography variant="body2" color="textSecondary"s>
                         {data.contactinfo}
                       </Typography>
-                      <Button size="small" color="primary">
+                      </CardContent>
+                    </CardActionArea>
+                      <CardActions>
+                      <Button size="small" className={classes.btn}>
                         Share
                       </Button>
-                      <Button size="small" color="primary">
+                      <Button size="small" className={classes.btn}>
                         Learn More
                       </Button>
-                    </CardContent>
-                  </CardActionArea>
+                      </CardActions>
                 </Card>
               </Grid>
               );
