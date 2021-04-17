699a4347804a4597418471480b67332b7a7c5569
Christopher Schiavino <Cschiavino23@gmail.com>
2021-04-17 15:30:57 -0400

    memes

--- frontend/src/TestData.js
+++ frontend/src/TestData.js
@@ -1,122 +1,102 @@
 export const TestData = [
     {
-        title: "bullshit",
-        description: "pp",
-        price: "100000.00",
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
         location: "Deez",
         contactinfo: "deez@gmail.com"
     },
     {
-        title: "bullshit1",
-        description: "pp1",
-        price: "6969.420",
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez1",
-        contactinfo: "beez1@gmail.com1"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
+    },{
+        title: "stinky dude",
+        description: "this dude stink!",
+        price: "free",
         picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
-    },
-    {
-        title: "poopoo2",
-        description: "sucky suck",
-        price: "2000.002",
-        picture: "",
-        location: "Deez2",
-        contactinfo: "peez1@gmail.com"
+        location: "Deez",
+        contactinfo: "deez@gmail.com"
     },
 ]
