667d35b8d0119c207cbcbd717a902e844a90a50f
Yousef Edwards <47361394+Yousef4410@users.noreply.github.com>
2021-04-23 13:30:34 -0400

    Delete MobileApp directory
    No mobile app since mobile frontend team was not able to create one

--- MobileApp/App.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Navigator from "./components/Navigator";
-
-export default function App() {
-  return <Navigator />;
-}
-
-const styles = StyleSheet.create({});
--- MobileApp/constants/colors.js
+++
@@ -1,8 +0,0 @@
-export default {
-  primary: "#334B68", // dark blue
-  accent: "#56F6E4", // neon blue
-  accent2: "#0E788F", // nice color on the login screen
-  cardColor: "#DBF2FF", // for contrast
-  white: "white", // for convenience
-  black: "black",
-};
